cfb0020c5bd57508e22fe6406f8fbb8fe37ee057
danlorb <danlorb@velvet-lab.net>
2026-06-22 18:01:20 +0200

    feat(extensions): add variable commands to configurator
    Introduce AddVariableCommands method to enhance configurator with variable command support, allowing users to list configured application variables.

--- libs/xSdk/src/Extensions/Variable/Commands/ConfiguratorExtensions.cs
+++ libs/xSdk/src/Extensions/Variable/Commands/ConfiguratorExtensions.cs
@@ -18,19 +18,22 @@ using Spectre.Console.Cli;
 
 namespace xSdk.Extensions.Variable.Commands;
 
-public static class CommandsExtensions
+public static class ConfiguratorExtensions
 {
-    public static IConfigurator AddVariableCommands(this IConfigurator configurator)
+    extension(IConfigurator builder)
     {
-        configurator.AddBranch(
-            "variable",
-            x =>
-            {
-                x.SetDescription("Helps to show current configured application variables");
-                x.AddCommand<ListCommand>(ListCommand.Definitions.Name).WithExample("variable", "list", "--show-help");
-            }
-        );
+        public IConfigurator AddVariableCommands()
+        {
+            builder.AddBranch(
+                "variable",
+                x =>
+                {
+                    x.SetDescription("Helps to show current configured application variables");
+                    x.AddCommand<ListCommand>(ListCommand.Definitions.Name).WithExample("variable", "list", "--show-help");
+                }
+            );
 
-        return configurator;
+            return builder;
+        }
     }
 }
